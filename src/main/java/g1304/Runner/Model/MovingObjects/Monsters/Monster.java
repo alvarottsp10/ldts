be19e85b0085172254dcfa6934d5852408d6d7da
alvarottsp10 <up202207733@up.pt>
2023-12-19 23:14:11 +0000

    classes orc,java,slime

--- src/main/java/g1304/Runner/Model/MovingObjects/Monsters/Monster.java
+++ src/main/java/g1304/Runner/Model/MovingObjects/Monsters/Monster.java
@@ -1,4 +1,64 @@
 package g1304.Runner.Model.MovingObjects.Monsters;
 
+import g1304.Runner.Model.MovingObjects.Position;
+
 public class Monster {
+    private Position position;
+
+    int lives = 1;
+
+    int damage;
+
+    private String LastMovement = "Down";
+
+
+    public static final String[] MonsterModel = {
+            "        __  ",
+            "      /    \\",
+            "     | o  o |",
+            "      \\ -- /",
+            "     __|  |__",
+            "   /          \\",
+            "  |            |",
+            "   \\  \\  /  /",
+            "    \\__\\/__/",
+    };
+
+
+    public Monster(Position position1, int level) {
+        position = position1;
+        damage = level;
+    }
+    public int getX() {
+        return position.getX();
+    }
+
+    public int getY() {
+        return position.getY();
+    }
+
+    public Position getPosition() {
+        return position;
+    }
+
+    public void setLastMovement(String lastMovement) {
+        LastMovement = lastMovement;
+    }
+
+    public String getLastMovement() {
+        return LastMovement;
+    }
+
+    public boolean isDead() {
+        return lives <= 0;
+    }
+
+    public int monsterDamage() {
+        return damage;
+    }
+
+    public void DamageMonster(int damage) {
+        lives -= damage;
+    }
+
 }
--- src/main/java/g1304/Runner/Model/MovingObjects/Monsters/Orc.java
+++ src/main/java/g1304/Runner/Model/MovingObjects/Monsters/Orc.java
@@ -1,4 +1,65 @@
 package g1304.Runner.Model.MovingObjects.Monsters;
 
+import g1304.Runner.Model.MovingObjects.Position;
+
 public class Orc {
+
+    private Position position;
+
+    int lives = 1;
+
+    int damage;
+
+    private String LastMovement = "Down";
+
+
+    public static final String[] OrcModel = {
+
+            "     ,--._____ ,",
+            "   ,--         \\`-.",
+            "  ,        (   ) |  `",
+            " ,   (`  (  )   )  `",
+            ",   /  ` ----- ' \\   `",
+            "|  /  /  /  /  / |",
+            "` /  /  /  /  /  '",
+            " `  |  | |  |  | '",
+            "  `--' `-+-`--' '",
+            "         |_|",
+    };
+    public Orc(Position position1, int level) {
+        position = position1;
+        damage = level;
+    }
+    public int getX() {
+        return position.getX();
+    }
+
+    public int getY() {
+        return position.getY();
+    }
+
+    public Position getPosition() {
+        return position;
+    }
+
+    public void setLastMovement(String lastMovement) {
+        LastMovement = lastMovement;
+    }
+
+    public String getLastMovement() {
+        return LastMovement;
+    }
+
+    public boolean isDead() {
+        return lives <= 0;
+    }
+
+    public int orcDamage() {
+        return damage;
+    }
+
+    public void DamageOrc(int damage) {
+        lives -= damage;
+    }
+
 }
